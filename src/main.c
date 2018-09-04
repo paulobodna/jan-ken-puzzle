e68d47f00ae381e1e1cd144926f2cc1a03bb7c7d
paulobodna <paulobodna@hotmail.com>
2018-09-04 00:12:08 -0300

    Code finished

--- src/main.c
+++ src/main.c
@@ -19,6 +19,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct endcase {
+    int row, col, piece;
+} ENDCASE;
+
+typedef struct solution {
+    int qtd_sol;
+    int qtd_cases;
+    ENDCASE *cases;
+} SOLUTION;
+
 void create_matrix(int ***matrix, int row, int col) {
 
     (*matrix) = (int **) malloc(row * sizeof(int *));
@@ -66,9 +76,24 @@ void delete_matrix(int ***matrix, int row) {
     return;
 }
 
-int recursive_play(int **matrix, int row, int col, int actual_row, int actual_col);
+int recursive_play(int **matrix, int row, int col, int actual_row, int actual_col, SOLUTION *sol);
 
-void move_to(int **matrix, int row, int col, int actual_row, int actual_col, int new_row, int new_col) {
+// verifica se ha uma solucao identica ja encontrada
+int new_case(SOLUTION *sol, int new_row, int new_col, int piece) {
+    for(int i = 0; i < sol->qtd_cases; i++) {
+        if(sol->cases[i].row == new_row &&
+            sol->cases[i].col == new_col &&
+            sol->cases[i].piece == piece) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
+
+// realiza o movimento requisitado recursivamente,
+// desfazendo-o ao retornar
+void move_to(int **matrix, int row, int col, int actual_row, int actual_col, int new_row, int new_col, SOLUTION *sol) {
     int piece = matrix[actual_row][actual_col];
 
     // fazendo o movimento
@@ -78,7 +103,7 @@ void move_to(int **matrix, int row, int col, int actual_row, int actual_col, int
     int qtd_pieces = 0;
     for(int i = 0; i < row; i++) {
         for(int j = 0; j < col; j++) {
-            qtd_pieces += recursive_play(matrix, row, col, i, j);
+            qtd_pieces += recursive_play(matrix, row, col, i, j, sol);
         }
     }
 
@@ -86,15 +111,26 @@ void move_to(int **matrix, int row, int col, int actual_row, int actual_col, int
     matrix[actual_row][actual_col] = piece;
     matrix[new_row][new_col] = (piece%3)+1;
 
-    // imprimir caso a resposta tenha sido encontrada
+    // salvar caso a resposta tenha sido encontrada
     if(qtd_pieces == 1) {
-        printf("%d %d %d\n", new_row+1, new_col+1, piece);
+        if(new_case(sol, new_row, new_col, piece)) {
+            sol->cases = realloc(sol->cases, sizeof(ENDCASE) * (sol->qtd_cases+1));
+            sol->cases[sol->qtd_cases].row = new_row;
+            sol->cases[sol->qtd_cases].col = new_col;
+            sol->cases[sol->qtd_cases].piece = piece;
+
+
+            sol->qtd_cases++;
+        }
+
+        sol->qtd_sol++;
     }
 
     return;
 }
 
-int recursive_play(int **matrix, int row, int col, int actual_row, int actual_col) {
+// realiza as jogadas recursivamente
+int recursive_play(int **matrix, int row, int col, int actual_row, int actual_col, SOLUTION *sol) {
     if(matrix[actual_row][actual_col] == 0) {
         return 0;
     }
@@ -103,34 +139,34 @@ int recursive_play(int **matrix, int row, int col, int actual_row, int actual_co
 
     // -> ir para direita
     if(actual_col < col-1 && matrix[actual_row][actual_col+1] == (piece%3)+1) {
-        move_to(matrix, row, col, actual_row, actual_col, actual_row, actual_col+1);
+        move_to(matrix, row, col, actual_row, actual_col, actual_row, actual_col+1, sol);
     }
 
     // -> ir para baixo
     if(actual_row < row-1 && matrix[actual_row+1][actual_col] == (piece%3)+1) {
-        move_to(matrix, row, col, actual_row, actual_col, actual_row+1, actual_col);
+        move_to(matrix, row, col, actual_row, actual_col, actual_row+1, actual_col, sol);
     }
     
     // -> ir para esquerda
     if(actual_col > 0 && matrix[actual_row][actual_col-1] == (piece%3)+1) {
-        move_to(matrix, row, col, actual_row, actual_col, actual_row, actual_col-1);
+        move_to(matrix, row, col, actual_row, actual_col, actual_row, actual_col-1, sol);
     }
 
     // -> ir para cima
     if(actual_row > 0 && matrix[actual_row-1][actual_col] == (piece%3)+1) {
-        move_to(matrix, row, col, actual_row, actual_col, actual_row-1, actual_col);
+        move_to(matrix, row, col, actual_row, actual_col, actual_row-1, actual_col, sol);
     }
 
     return 1;
 }
 
-void find_solutions(int **matrix, int row, int col) {
+void find_solutions(int **matrix, int row, int col, SOLUTION *sol) {
     
     int qtd_pieces = 0;
     for(int i = 0; i < row; i ++) {
         for(int j = 0; j < col; j++) {
             if(matrix[i][j] != 0) {
-                qtd_pieces += recursive_play(matrix, row, col, i, j);
+                qtd_pieces += recursive_play(matrix, row, col, i, j, sol);
             }
         }
     }
@@ -142,21 +178,69 @@ void find_solutions(int **matrix, int row, int col) {
     return;
 }
 
+void create_sol(SOLUTION **sol) {
+
+    (*sol) = (SOLUTION *) malloc(sizeof(SOLUTION));
+
+    (*sol)->qtd_sol = 0;
+    (*sol)->qtd_cases = 0;
+    (*sol)->cases = NULL;
+
+    return;
+}
+
+void delete_sol(SOLUTION **sol) {
+    free((*sol)->cases);
+    free((*sol));
+
+    (*sol) = NULL;
+
+    return;
+}
+
+void print_sol (SOLUTION *sol) {
+    printf("%d\n%d\n", sol->qtd_sol, sol->qtd_cases);
+    for(int i = 0; i < sol->qtd_cases; i++) {
+        printf("%d %d %d\n", sol->cases[i].row,
+                            sol->cases[i].col,
+                            sol->cases[i].piece);
+    }
+
+    return;
+}
+
+// funcao de comparacao para ordenar casos
+int comp(const void *elem1, const void *elem2) {
+
+    ENDCASE f = *((ENDCASE *)elem1);
+    ENDCASE s = *((ENDCASE *)elem2);
+
+    if(f.row <= s.row && f.col <= s.col && f.piece < s.piece) return -1;
+    else return 1;
+
+    return 0;
+}
+
 int main(int argc, char* argv[]) {
 
     int row, col;
     scanf("%d %d", &row, &col);
 
     int **matrix;
+    SOLUTION *sol;
 
     create_matrix(&matrix, row, col);
+    create_sol(&sol);
+
     read_matrix(matrix, row, col);
-    print_matrix(matrix, row, col);
 
-    find_solutions(matrix, row, col);
+    find_solutions(matrix, row, col, sol);
+    // ordenar casos
+    qsort(sol->cases, sol->qtd_cases, sizeof(ENDCASE), comp);
+    print_sol(sol);
 
+    delete_sol(&sol);
     delete_matrix(&matrix, row);
-
 
     return 0;
 }
